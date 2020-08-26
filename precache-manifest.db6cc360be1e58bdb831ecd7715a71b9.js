b0050b1650eeb9530a635959c37c4d7449c9e0af
LEvinson2504 <ldsouza2504@gmail.com>
2020-08-26 16:36:38 +0530

    Updates

---
+++ precache-manifest.db6cc360be1e58bdb831ecd7715a71b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d30ad618151733fd6aac5a6cef4e93",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "ff03a9b353f869951452",
+    "url": "/MyPortfolio/static/css/main.98e50b04.chunk.css"
+  },
+  {
+    "revision": "fa59470ffebae1787931",
+    "url": "/MyPortfolio/static/js/2.158e4496.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyPortfolio/static/js/2.158e4496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff03a9b353f869951452",
+    "url": "/MyPortfolio/static/js/main.18a4abf4.chunk.js"
+  },
+  {
+    "revision": "57cbf6c69aa92ff59cd2",
+    "url": "/MyPortfolio/static/js/runtime-main.59479f4c.js"
+  }
+]);
